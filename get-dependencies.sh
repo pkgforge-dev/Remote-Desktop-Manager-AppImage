1054846c394258f658841f5ebc6c8528483e3e82
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-14 02:39:48 -0400

    remove broken `ssh` binary

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -22,3 +22,9 @@ cp -rv /usr/lib/devolutions/RemoteDesktopManager/* ./AppDir/bin
 # app has a helper library that links to webkit2gtk4 instead of 4.1
 # this library is 100% broken and prevents deployment
 rm -f ./AppDir/bin/libWebView-4.0.so*
+
+# this app ships its own ssh binary 🤨
+# it depends on an outdated libcrypt.so.1
+# it is totally broken on archlinux by default
+# remove it, but I'm starting to think this app is totally broken in general
+rm -f ./AppDir/bin/ssh
