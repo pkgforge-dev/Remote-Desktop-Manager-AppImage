120ba63f3422c93496490c681481dbf6f4f97e99
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-13 22:51:51 -0400

    remote broken lib

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,3 +18,7 @@ make-aur-package remote-desktop-manager
 # If the application needs to be manually built that has to be done down here
 mkdir -p ./AppDir/bin
 cp -rv /usr/lib/devolutions/RemoteDesktopManager/* ./AppDir/bin
+
+# app has a helper library that links to webkit2gtk4 instead of 4.1
+# this library is 100% broken and prevents deployment
+rm -f ./AppDir/bin/libWebView-4.0.so*
