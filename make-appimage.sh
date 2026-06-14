afb9f386b25cd72b8e00cf75875ead32a7980579
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-13 22:38:42 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q remote-desktop-manager | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.hook"
+export ADD_HOOKS="self-updater.hook:wayland-is-broken" # upstream forces GDK_BACKEND=x11 in their launcher script
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/com.devolutions.remotedesktopmanager.desktop
+export DESKTOP=/usr/share/applications/com.devolutions.remotedesktopmanager.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun $(find ./AppDir/bin -maxdepth 1 ! -name '*.dll' -print)
 
 # Additional changes can be done in between here
 
